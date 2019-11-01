a9ac1c293e88a342c86405a31764846a19f2d63a
KRIEI <kristoffer.eide@dnvgl.com>
2019-11-01 13:30:33 +0100

    Set hardcoded version to 0.1.1

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char const* const* argv)
         "Command-line interface to the Core Simulation Environment",
         "The Core Simulation Environment is free and open-source software for running distributed co-simulations.");
     app.add_global_options(std::make_unique<logging_options>());
-    app.add_global_options(std::make_unique<version_option>("CSE CLI", "0.2.0"));
+    app.add_global_options(std::make_unique<version_option>("CSE CLI", "0.1.1"));
     app.add_subcommand(std::make_unique<inspect_subcommand>());
     app.add_subcommand(std::make_unique<run_subcommand>());
     app.add_subcommand(std::make_unique<run_single_subcommand>());
