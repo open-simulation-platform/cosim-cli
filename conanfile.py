a41825ba73950fdf01a77bc242249a8573afa3d8
Lars T. Kyllingstad <lars.kyllingstad@sintef.no>
2023-11-23 12:36:14 +0100

    Iterate over dependencies, don't specify manually

--- conanfile.py
+++ conanfile.py
@@ -17,10 +17,14 @@ class CosimCLIConan(ConanFile):
         self.requires("boost/[>=1.71]")
 
     def generate(self):
+        # Import shared libraries and executables from dependency packages
+        # to the 'dist/' folder.
         bindir = os.path.join(self.build_folder, "dist", "bin")
         dldir = (bindir if self.settings.os == "Windows" else
             os.path.join(self.build_folder, "dist", "lib"))
-        self._import_dynamic_libs("boost", dldir, [
+        dependency_libs = {
+            # For some dependencies, we only want a subset of the libraries
+            "boost" : [
                 "boost_atomic*",
                 "boost_chrono*",
                 "boost_container*",
@@ -35,38 +39,36 @@ class CosimCLIConan(ConanFile):
                 "boost_regex*",
                 "boost_serialization*",
                 "boost_system*",
-                "boost_thread*",
-            ])
-        self._import_dynamic_libs("libcosim", dldir)
-        self._import_dynamic_libs("openssl", dldir)
-        self._import_dynamic_libs("fmilibrary", dldir)
-        self._import_dynamic_libs("xerces-c", dldir)
-        self._import_dynamic_libs("yaml-cpp", dldir)
-        self._import_dynamic_libs("libzip", dldir)
+                "boost_thread*"],
+            "thrift": ["thrift", "thriftd"],
+        }
+        for req, dep in self.dependencies.items():
+            self._import_dynamic_libs(dep, dldir, dependency_libs.get(req.ref.name, ["*"]))
         if self.dependencies["libcosim"].options.proxyfmu:
-            self._import_dynamic_libs("proxyfmu", dldir)
-            self._import_dynamic_libs("thrift", dldir, ["thrift", "thriftd"])
-            self._import_executables("proxyfmu", bindir)
+            self._import_executables(self.dependencies["proxyfmu"], bindir, ["*"])
+
+        # Generate build system
         CMakeToolchain(self).generate()
         CMakeDeps(self).generate()
 
-    def _import_executables(self, dependency_name, target_dir, exenames=["*"]):
-        dep = self.dependencies[dependency_name]
-        for bindir in dep.cpp_info.bindirs:
-            for exename in exenames:
-                pattern = exename+".exe" if self.settings.os == "Windows" else exename
-                files = copy(self, pattern, bindir, target_dir, keep_path=False)
-                self._update_rpath(files, "$ORIGIN/../lib")
-
-    def _import_dynamic_libs(self, dependency_name, target_dir, libnames=["*"]):
-        dep = self.dependencies[dependency_name]
-        if dep.options.get_safe("shared", False):
-            depdirs = dep.cpp_info.bindirs if self.settings.os == "Windows" else dep.cpp_info.libdirs
+    def _import_dynamic_libs(self, dependency, target_dir, patterns):
+        if dependency.options.get_safe("shared", False):
+            if self.settings.os == "Windows":
+                depdirs = dependency.cpp_info.bindirs
+            else:
+                depdirs = dependency.cpp_info.libdirs
             for depdir in depdirs:
-                for libname in libnames:
-                    pattern = libname+".dll" if self.settings.os == "Windows" else "lib"+libname+".so*"
-                    files = copy(self, pattern, depdir, target_dir, keep_path=False)
+                for pattern in patterns:
+                    patternx = pattern+".dll" if self.settings.os == "Windows" else "lib"+pattern+".so*"
+                    files = copy(self, patternx, depdir, target_dir, keep_path=False)
                     self._update_rpath(files, "$ORIGIN")
+
+    def _import_executables(self, dependency, target_dir, patterns=["*"]):
+        for bindir in dependency.cpp_info.bindirs:
+            for pattern in patterns:
+                patternx = pattern+".exe" if self.settings.os == "Windows" else pattern
+                files = copy(self, patternx, bindir, target_dir, keep_path=False)
+                self._update_rpath(files, "$ORIGIN/../lib")
 
     def _update_rpath(self, files, new_rpath):
         if files and self.settings.os == "Linux":
